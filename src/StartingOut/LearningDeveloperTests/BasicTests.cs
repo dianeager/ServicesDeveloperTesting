01c10adde0e8b42b123c78e6fc47da596bf08ce7
Diane Ager <diane_ager@progressive.com>
2024-05-20 11:50:30 -0400

    create commit

---
+++ src/StartingOut/LearningDeveloperTests/BasicTests.cs
@@ -0,0 +1,32 @@
+﻿
+namespace LearningDeveloperTests;
+public class BasicTests
+{
+    //Using XUnit
+    [Fact]
+    public void CanAddTenAndTwenty()
+    {
+        //Arrange
+        int a = 10, b = 20;
+        int c;
+
+        //Act
+        c = a + b;
+
+        //Assert
+        //if (c != 30)
+        //{
+        //    throw new Exception("Can't Add!");
+        //}
+        Assert.Equal(30, c);
+    }
+    [Theory]
+    [InlineData(10, 20, 30)]
+    [InlineData(4, 2, 6)]
+    public void CanAddAnyTwoIntegers(int a, int b, int expected)
+    {
+        int answer = a + b;
+
+        Assert.Equal(expected, answer);
+    }
+}
